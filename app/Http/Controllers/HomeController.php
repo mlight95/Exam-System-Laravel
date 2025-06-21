018100cc2541294d7f2054d67ebcc054dafb4907
mlight <moon0905light@gmail.com>
2025-06-21 19:03:12 +0900

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,6 +27,9 @@ class HomeController extends Controller
     }
 
     public function goDashboard() {
-        
+        $requests = 90;
+        if($requests > 10 ){
+            $added = 10;
+        }
     }
 }
