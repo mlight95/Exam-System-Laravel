c9475756a30b2fca2a6c3ff4275e1057a43d7ac9
mlight <moon0905light@gmail.com>
2025-06-24 07:56:33 +0900

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,6 +30,8 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function goDashboard() {
+    public function goDashboard(Request $request) {
+        $requests = $request->all();
+        $data = [];
     }
 }
