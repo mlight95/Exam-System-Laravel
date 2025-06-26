d2a0705f31d271dd1f8cbd1e773681f57938b1ed
mlight <moon0905light@gmail.com>
2025-06-26 14:25:53 +0900

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,7 @@ class HomeController extends Controller
     {
         return view('home');
     }
-/**
+    /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Http\Response
