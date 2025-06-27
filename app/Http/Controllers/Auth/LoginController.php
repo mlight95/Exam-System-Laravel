0013b8ac81eb4762b8a0201805079e37c51d2e5d
mlight <moon0905light@gmail.com>
2025-06-27 19:14:16 +0900

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -23,7 +23,7 @@ class LoginController extends Controller
     /**
      * Where to redirect users after login.
      *
-     * @var string request
+     * @var string 
      */
     protected $redirectTo = '/home';
 
