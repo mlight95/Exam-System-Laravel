661f4c59d8e5748918de47c4d4ad8fc440eb95f0
mlight <moon0905light@gmail.com>
2025-07-01 18:07:39 +0900

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -86,7 +86,6 @@
                     <a href="https://laravel.com/docs">Document</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
