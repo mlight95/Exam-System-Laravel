74ce551a8da91116b1824bdaea4e44b6ea873350
mlight <moon0905light@gmail.com>
2025-07-01 18:08:05 +0900

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,7 +84,6 @@
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Document</a>
-                    <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
