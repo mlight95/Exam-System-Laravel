cd9f5963797681479c0c98de2b370d10c7512cd4
mlight <moon0905light@gmail.com>
2025-07-02 17:57:08 +0900

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome Developers
+                    Welcome
                 </div>
 
                 <div class="links">
