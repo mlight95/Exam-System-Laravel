bacef8176a5e5de9e3c7512762ce03d92519e342
jason-gidwani <moon0905light@gmail.com>
2025-07-15 22:30:57 +0900

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,18 +19,3 @@
     </div>
 </div>
 @endsection
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Home</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                </div>
-            </div>
-        </div>
-    </div>
